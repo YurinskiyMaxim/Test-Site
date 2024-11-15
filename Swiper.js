d49890212480e2b8ae4580a10feb3455c258a98a
WorldSkills 39 <ws39@chtotib.ru>
2024-11-15 13:37:26 +0900

    tablet_add

--- Swiper.js
+++ Swiper.js
@@ -1,15 +1,15 @@
 const swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
+  slidesPerView: "auto",
 
-    loop: true,
-    breakpoints: {
-      1119: {
-        slidesPerColumn: 2,
-        autoHeight: true,
-        loop: false,
-      },
+  loop: true,
+  breakpoints: {
+    1119: {
+      slidesPerColumn: 2,
+      autoHeight: true,
+      loop: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
